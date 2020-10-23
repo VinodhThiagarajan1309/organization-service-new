07eea5f2096912423bb04a2c480c377d7c40f626
Vinodh Thiagarajan <vinodh.thiagarajan@gmail.com>
2020-10-23 10:01:18 -0500

    Made this the New org service

--- src/main/java/com/vinapex/organizationservicenew/ApexOrganizationServiceNewApplication.java
+++ src/main/java/com/vinapex/organizationservicenew/ApexOrganizationServiceNewApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApexOrganizationServiceApplication {
+public class ApexOrganizationServiceNewApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApexOrganizationServiceApplication.class, args);
+        SpringApplication.run(ApexOrganizationServiceNewApplication.class, args);
     }
 }
